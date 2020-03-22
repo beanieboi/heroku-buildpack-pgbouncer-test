596dbd09f0ee86a97decb431a5c73da6465cabef
Ben Fritsch <beanie@benle.de>
2020-03-22 13:52:56 +0100

    add migrations

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'sequel'
+require 'pg'
+
+DB = Sequel.connect(ENV['DATABASE_URL'])
+
+DB.create_table :items do
+  primary_key :id
+  String :name
+  Float :price
+end
+
+items = DB[:items]
+items.insert(name: 'abc', price: rand * 100)
+items.insert(name: 'def', price: rand * 100)
+items.insert(name: 'ghi', price: rand * 100)
 
 get '/' do
   "Hello World!"
