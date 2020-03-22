3acfa108c2ae16733a55636b833cbf8bd53887ba
Ben Fritsch <beanie@benle.de>
2020-03-22 14:12:22 +0100

    create table only once

--- app.rb
+++ app.rb
@@ -4,16 +4,18 @@ require 'pg'
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
-DB.create_table :items do
-  primary_key :id
-  String :name
-  Float :price
-end
+unless DB.table_exists?(:items)
+  DB.create_table(:items) do
+    primary_key :id
+    String :name
+    Float :price
+  end
 
-items = DB[:items]
-items.insert(name: 'abc', price: rand * 100)
-items.insert(name: 'def', price: rand * 100)
-items.insert(name: 'ghi', price: rand * 100)
+  items = DB[:items]
+  items.insert(name: 'abc', price: rand * 100)
+  items.insert(name: 'def', price: rand * 100)
+  items.insert(name: 'ghi', price: rand * 100)
+end
 
 get '/' do
   "Hello World!"
